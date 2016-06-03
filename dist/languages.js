25fd1a4139232f0ecc013ab6d7f0e3e5e19c1e7e
Aseem Sood <asood123@yahoo.com>
2016-06-03 18:00:32 -0400

    generating dist files

--- dist/languages.js
+++ dist/languages.js
@@ -420,6 +420,60 @@
 
 /*!
  * numbro.js language configuration
+ * language : Uruguay Spanish
+ * locale: Uruguay
+ * author : Aseem Sood : http://github.com/asood123
+ */
+(function () {
+    'use strict';
+
+    var language = {
+        langLocaleCode: 'es-UR',
+        cultureCode: 'es-UR',
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'mm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 3) ? 'er' :
+                (b === 2) ? 'do' :
+                (b === 7 || b === 0) ? 'mo' :
+        (b === 8) ? 'vo' :
+        (b === 9) ? 'no' : 'to';
+        },
+        currency: {
+            symbol: '$U',
+            position: 'postfix'
+        },
+        defaults: {
+            currencyFormat: ',4 a'
+        },
+        formats: {
+            fourDigits: '4 a',
+            fullWithTwoDecimals: ',0.00 $U',
+            fullWithTwoDecimalsNoCurrency: ',0.00',
+            fullWithNoDecimals: ',0 $U'
+        }
+    };
+
+    // CommonJS
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && window.numbro && window.numbro.culture) {
+        window.numbro.culture(language.cultureCode, language);
+    }
+}.call(typeof window === 'undefined' ? this : window));
+/*!
+ * numbro.js language configuration
  * language : Estonian
  * locale: Estonia
  * author : Illimar Tambek : https://github.com/ragulka
@@ -632,7 +686,8 @@
         },
         currency: {
             symbol: '$',
-            position: 'prefix'
+            position: 'postfix',
+            spaceSeparated : true
         },
         defaults: {
             currencyFormat: ',4 a'
@@ -803,6 +858,59 @@
 }.call(typeof window === 'undefined' ? this : window));
 
 
+/*!
+ * numbro.js language configuration
+ * language : Indian English
+ * locale: India
+ * author : Aseem Sood : http://github.com/asood123
+ */
+(function () {
+    'use strict';
+
+    var language = {
+        langLocaleCode: 'hi-IN',
+        cultureCode: 'hi-IN',
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal : function (number) {
+            var b = number % 10;
+            return (~~ (number % 100 / 10) === 1) ? 'th' :
+                (b === 1) ? 'st' :
+                (b === 2) ? 'nd' :
+                (b === 3) ? 'rd' : 'th';
+        },
+        currency: {
+            symbol: '₹ ',
+            position: 'prefix'
+        },
+        defaults: {
+            currencyFormat: ',4 a'
+        },
+        formats: {
+            fourDigits: '4 a',
+            fullWithTwoDecimals: ',₹ 0.00',
+            fullWithTwoDecimalsNoCurrency: ',0.00',
+            fullWithNoDecimals: ',₹ 0'
+        }
+    };
+
+    // CommonJS
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && window.numbro && window.numbro.culture) {
+        window.numbro.culture(language.cultureCode, language);
+    }
+}.call(typeof window === 'undefined' ? this : window));
 /*!
  * numbro.js language configuration
  * language : Hungarian
@@ -1268,7 +1376,7 @@
         },
         currency: {
             symbol: 'R$',
-            position: 'postfix'
+            position: 'prefix'
         },
         defaults: {
             currencyFormat: ',4 a'
